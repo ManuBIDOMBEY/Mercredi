98e3ffc65bc35afe4d9f02eb2dc295cce4fb1ad4
lessan gwladys ADOM <lessangwladysadom@gmail.com>
2025-04-02 17:04:39 +0200

    NavBar

---
+++ src/Partiel/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div style={{ display: 'flex', flexDirection: 'row', gap: '50px', backgroundColor: 'write' }}>
+
+            <a href="http://accueil">Acceuil</a>
+            <a href="http://profile">Profile</a>
+            <a href="http://form,">Form</a>
+
+        </div>
+    )
+}
+
+export default NavBar
